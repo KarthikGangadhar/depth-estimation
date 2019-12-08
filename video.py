b19bc82ad03887e94cebb185d8157a9a9f7e0543
kgangadhar <karthikg1643@gmail.com>
2019-12-08 17:14:00 -0600

    added argument changes

--- video.py
+++ video.py
@@ -43,25 +43,30 @@ def display_single_image(output, inputs=None, is_colormap=True):
     img_set = np.hstack(imgs)
     return img_set
 
-custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': depth_loss_function }
-print('Loading model...')
-
-# Load the saved model
-model_name = 'nyu.h5'
-model = load_model(model_name, custom_objects=custom_objects, compile=False)
-print('\nModel loaded ({0}).'.format(model_name))
-
-while True:
-    (runnin, frame) = video.read()
-    im = Image.fromarray(frame, 'RGB')
-    img_array = np.array(im)
-    img_arr = get_img_arr(img_array)
-    output = scale_up(2, predict(model, img_arr, batch_size=1))
-    pred = output.reshape(output.shape[1], output.shape[2], 1)
-    img_set = display_single_image(pred, img_arr)
-    cv2.imshow('Capturing', img_set)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+	args = sys.argv[ 1: ]
+	if (len(args) <= 0) :
+		sys.exit( 0 )
+	model_name = args[0]
+	
+	custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': depth_loss_function }
+	# Load the saved model
+	print('Loading model...')
+	# model_name = 'nyu.h5'
+	model = load_model(model_name, custom_objects=custom_objects, compile=False)
+	print('\nModel loaded ({0}).'.format(model_name))
+	
+	while True:
+	    (runnin, frame) = video.read()
+	    im = Image.fromarray(frame, 'RGB')
+	    img_array = np.array(im)
+	    img_arr = get_img_arr(img_array)
+	    output = scale_up(2, predict(model, img_arr, batch_size=1))
+	    pred = output.reshape(output.shape[1], output.shape[2], 1)
+	    img_set = display_single_image(pred, img_arr)
+	    cv2.imshow('Capturing', img_set)
+	    key = cv2.waitKey(1)
+	    if key == ord('q'):
+	        break
+	video.release()
+	cv2.destroyAllWindows()
